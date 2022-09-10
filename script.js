b4c2ab6d34c8181a0e175953e95060bc4c40db50
Gilreon <58603241+Gilreon@users.noreply.github.com>
2022-09-10 13:20:24 -0500

    completed js ratings

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
-let button = document.getElementsByClassName("rating-btn")
-let submitButton = document.getElementsByClassName("submitBtn")
 let thanksContainer = document.getElementsByClassName("containerSubmit")
 let mainContainer = document.getElementsByClassName("container")
-
+let button = document.getElementsByClassName("rating-btn")
+let submitButton = document.getElementsByClassName("submitBtn")
+let actualRating = document.getElementById('rating')
+let ratings = document.querySelectorAll('.btn')
 
 let addSelectClass = function(){
     removeSelectClass();
     this.classList.add('selected');
+    
 }
 
 let removeSelectClass = function() {
@@ -18,12 +19,16 @@ let removeSelectClass = function() {
 
 for (let i =0; i < button.length; i++){
     button[i].addEventListener("click",addSelectClass)
+
 }
 
 submitButton[0].addEventListener("click", () => {
     thanksContainer[0].classList.remove('hidden')
-    mainContainer[0].classList.add('hidden')
+    mainContainer[0].style.display = "none"
+});
+
+ratings.forEach((rating) => {
+    rating.addEventListener("click", () => {
+        actualRating.innerHTML = rating.innerHTML
+    })
 })
-    
-
-
